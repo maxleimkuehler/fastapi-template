85f0baf96418ff5bbfe406f2e96b34823b7806ef
maxleimkuehler <max.leimkuehler@gmail.com>
2020-11-30 13:55:56 +0100

    add simple test

--- src/controller/test_simple_example.py
+++ src/controller/test_simple_example.py
@@ -8,3 +8,8 @@ def test_init():
     simpleExample = _setup()
 
     assert hasattr(simpleExample, 'multiplier')
+
+def test_get_result():
+    simpleExample = _setup()
+
+    assert simpleExample.get_result(5) == 25
